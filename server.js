8d513cd47e3dc4b6026c21ea727abe216e142881
Oleg Shiliaev <shyliaievoleg@gmail.com>
2019-05-27 12:25:38 +0300

    fix server path

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 
 const port = process.env.PORT || 3000;
+
+app
+  .use(express.static(path.join(__dirname, 'public')))
+  .use(express.json())
+  .use(express.urlencoded({extended: false}));
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}...`);
