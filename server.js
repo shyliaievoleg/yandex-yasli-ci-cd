2adccc622ef360ac7e9c24e26c3f5d47e9f26967
Oleg Shiliaev <shyliaievoleg@gmail.com>
2019-05-27 12:25:38 +0300

    fix server path

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.listen(port, () => {
   console.log(`App listening on port ${port}...`);
 });
